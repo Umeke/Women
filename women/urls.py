e51d97f0d5a33813dda5d2992f60e0efccd051ab
Umeke <83427453+Umeke@users.noreply.github.com>
2024-07-25 15:26:55 +0600

    /first

---
+++ women/urls.py
@@ -0,0 +1,17 @@
+from .views import *
+from django.urls import path, re_path
+
+urlpatterns = [
+
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('login/', login, name='login'),
+    path('contact/', contact, name='contact'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name = 'category'),
+    #path('cats/', categories),
+    path('cats/<int:catid>', categories),
+    re_path(r'archive/(?P<year>[0-9]{4})', archive),
+]
+
---
+++ women/views.py
@@ -0,0 +1,53 @@
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+from .models import *
+menu = [{'title':"О сайте", 'url_name':'about'},
+        {'title':"Добавить статью", 'url_name':'add_page'},
+        {'title':"Обратная связь", 'url_name':'contact'},
+        {'title':"Войти", 'url_name':'login'}]
+def index(request):
+    posts = Women.objects.all()
+    cats = Category.objects.all()
+    context = {"menu":menu, "title":"Главный страница", "posts":posts, "cats":cats, "cat_selected":0,}
+    return render(request, "women/index.html",context=context)
+
+def about(request):
+    return render(request, "women/about.html", {"menu":menu, "title":"О сайте"})
+
+def addpage(request):
+    return HttpResponseNotFound('bet kosu')
+
+def login(request):
+    print(request.POST)
+    return HttpResponseNotFound('login')
+
+def contact(request):
+    return HttpResponseNotFound('contact')
+
+def categories(request, catid):
+    print(request.POST)
+    return HttpResponse(f'{catid}')
+
+def show_post(request, post_id):
+    return HttpResponse(f'{post_id}')
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+    if len(posts)==0:
+        raise Http404
+    context = {"menu": menu, "title": "Отображение по рубрикам", "posts": posts, "cats": cats, "cat_selected": cat_id, }
+    return render(request, "women/index.html", context=context)
+
+# def categories(request):
+#     return HttpResponse("222")
+
+def archive(request,year):
+    print(year)
+    if int(year)>2020:
+        raise Http404()
+        #return redirect('home', permanent=True)
+    return HttpResponse(f'{year}')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Бет табылмады<h1>')
