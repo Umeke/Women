fa1fb8444eca06f59a3fdb0085bb81363745997c
Umeke <83427453+Umeke@users.noreply.github.com>
2024-08-02 01:22:30 +0600

    women_tags

--- women/views.py
+++ women/views.py
@@ -7,8 +7,8 @@ menu = [{'title':"О сайте", 'url_name':'about'},
         {'title':"Войти", 'url_name':'login'}]
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
-    context = {"menu":menu, "title":"Главный страница", "posts":posts, "cats":cats, "cat_selected":0,}
+
+    context = {"menu":menu, "title":"Главный страница", "posts":posts, "cat_selected":0,}
     return render(request, "women/index.html",context=context)
 
 def about(request):
@@ -33,10 +33,10 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
+
     if len(posts)==0:
         raise Http404
-    context = {"menu": menu, "title": "Отображение по рубрикам", "posts": posts, "cats": cats, "cat_selected": cat_id, }
+    context = {"menu": menu, "title": "Отображение по рубрикам", "posts": posts,  "cat_selected": cat_id, }
     return render(request, "women/index.html", context=context)
 
 # def categories(request):
