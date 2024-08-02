b9538d1651d4c03d7d78396994df402c3fc2e732
Umeke <83427453+Umeke@users.noreply.github.com>
2024-08-02 23:12:43 +0600

    add post page

--- women/urls.py
+++ women/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('addpage/', addpage, name='add_page'),
     path('login/', login, name='login'),
     path('contact/', contact, name='contact'),
-    path('post/<int:post_id>/', show_post, name='post'),
+    path('post/<slug:post_slug>/', show_post, name='post'),
     path('category/<int:cat_id>/', show_category, name = 'category'),
     #path('cats/', categories),
     path('cats/<int:catid>', categories),
